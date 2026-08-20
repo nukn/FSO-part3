38397f19c5e094fe7743b66838430c411c894cbb
unknown <karitakors@gmail.com>
2026-08-20 12:32:04 +0300

    render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('cors')
+
 
 app.use(express.json())
 
@@ -10,7 +10,6 @@ morgan.token('body', (request) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
