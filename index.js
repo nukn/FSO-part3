268f905fb67de80d851e252bc1af89bc229a585b
unknown <karitakors@gmail.com>
2026-08-20 12:15:37 +0300

    render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
+const cors = require('cors')
 
 app.use(express.json())
 
@@ -10,6 +10,9 @@ morgan.token('body', (request) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
+
+app.use(cors())
 
 let persons = [
   {
@@ -94,7 +97,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
