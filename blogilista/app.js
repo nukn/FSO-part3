3f99c468d89fcc5d05b156dfdb8e6d028eb2c5cb
unknown <karitakors@gmail.com>
2026-08-21 00:53:25 +0300

    4.2

---
+++ blogilista/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
